ffba42386687838ab5f923f946ff223a8483e204
Emzi0767 <matiz100@windowslive.com>
2017-05-14 21:30:05 +0200

    Added MIPS and MIPS LE to help.

--- asmbot/__init__.py
+++ asmbot/__init__.py
@@ -6,7 +6,7 @@ A Discord bot by Emzi0767. Designed to quickly assemble x86, x86_64 and ARM asse
 
 __name__ = "asmbot"
 __author__ = "Emzi0767"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2017 Emzi0767"
 
